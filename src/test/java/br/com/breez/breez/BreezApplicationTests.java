16eedf1c260385bf9cacc232f1e510b1f6c3506d
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2025-06-02 00:09:08 -0300

    Adicionado h2 para testes

--- src/test/java/br/com/breez/breez/BreezApplicationTests.java
+++ src/test/java/br/com/breez/breez/BreezApplicationTests.java
@@ -2,10 +2,10 @@ package br.com.breez.breez;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BreezApplicationTests {
 
 	@Test
