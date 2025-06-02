88765a78f6467f783e51a10ea3b2f53db22133f4
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2025-06-01 21:52:22 -0300

    Testando exclusao de validacao do Pipelines

--- src/main/java/br/com/breez/breez/BreezApplication.java
+++ src/main/java/br/com/breez/breez/BreezApplication.java
@@ -3,11 +3,15 @@ package br.com.breez.breez;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration.class
+		}
+)
 public class BreezApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BreezApplication.class, args);
 	}
-
 }
